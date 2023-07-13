d3292dee0861486b5ca93f0116eaf7f27860cf27
Aditya <khadekaraditya20743@gmail.com>
2023-07-13 14:30:57 +0530

    add keyboard values done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import './App.css';
+import "./App.css";
 import { useState } from "react";
-import Board from './components/Board';
-import Keyboard from './components/Keyboard';
-import { boardDefault } from './Words';
-import { createContext} from 'react';
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./Words";
+import { createContext } from "react";
 
 export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+
+  const onSelectLetter = (KeyVal) => {
+    if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = KeyVal;
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+  };
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 });
+  };
+  const onEnter = () => {
+    if (currAttempt.letterPos !== 5) return;
+    setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+  };
+
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <AppContext.Provider  value= {{board, setBoard}}>
-        <Board />
-        <Keyboard/>
+      <AppContext.Provider
+        value={{ board, setBoard, currAttempt, setCurrAttempt, onSelectLetter,onDelete,onEnter }}
+      >
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
       </AppContext.Provider>
-      
     </div>
   );
 }
--- src/components/Key.js
+++ src/components/Key.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useContext,} from "react";
+import { AppContext } from "../App";
 
-function Key() {
+
+function Key({KeyVal , bigKey}) { 
+  const {onSelectLetter, onDelete, onEnter} = useContext(AppContext);
+
+  const selectLetter = () =>{
+    if(KeyVal==="ENTER"){
+      onEnter();
+    }
+    else if(KeyVal ==="DELETE"){
+      onDelete();
+    }
+    else{
+      onSelectLetter(KeyVal);
+    }
+  
+  }
   return (
-    <div>Key</div>
+    <div className="key" id={bigKey && "big"} onClick={selectLetter}>{KeyVal} </div>
   )
 }
 
-export default Key
+export default Key;
